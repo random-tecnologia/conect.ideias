62fdbe7d32bf7d276af8a55259463660545c8b70
riquekaique <rique.kaique@gmail.com>
2017-09-15 20:36:24 -0300

    Finalizado páginas dos protótipos para cadastro e login

---
+++ cadastro.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Conect.Ideias | Achar projetos</title>
+
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/main.css">
+  </head>
+  <body>
+    <div class="container">
+      <form action="cadastro.php" method="post" class="col-md-5">
+        <div class="form-group">
+          <label for="email-cadastro">E-mail</label>
+          <input type="email" class="form-control" id="email-cadastro" aria-describedby="emailHelp" placeholder="Digite seu e-mail">
+          <small id="emailHelp" class="form-text text-muted">Nós nunca compartilharemos seu e-mail.</small>
+        </div>
+        <div class="form-group">
+          <label for="senha-cadastro">Escolha uma senha</label>
+          <input type="password" class="form-control" id="senha-cadastro" placeholder="Digite sua senha">
+        </div>
+        <button type="submit" class="btn btn-primary">Criar conta</button>
+      </form>
+  </div>
+
+
+    <script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
