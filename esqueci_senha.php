074d76e3b27224991c36878dae7a8c5c510d17cb
Kaique Borges <rique.kaique@gmail.com>
2017-10-11 12:21:15 -0300

    Criadas páginas esqueci_senha.php e redefinir_senha.php

---
+++ esqueci_senha.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Conect.Ideias | Entrar</title>
+  <link rel="stylesheet" href="css/main.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i|Montserrat:400,500,600,700" rel="stylesheet"> 
+  <link rel="icon" href="img/favicon.png" sizes="32x32" type="image/png">
+</head>
+<body>
+  <img id="logo-entrar" src="img/logo_branco.png" alt="Logomarca Conect.Ideias">
+  <section class="container">
+    <h1>Recupere sua senha</h1>
+    <p>Enviaremos um e-mail para redefinir sua senha.</p>
+    <form class="formulario-entrar" action="" method="post">
+      <input type="text" name="email" placeholder="E-mail" required>
+      <a id="esqueci-senha" href="login.php">Voltar</a>
+      <input class="btn-primario" type="submit" name="submit" value="Enviar">
+    </form>
+  </section>
+</body>
+</html>
---
+++ redefinir_senha.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Conect.Ideias | Entrar</title>
+  <link rel="stylesheet" href="css/main.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i|Montserrat:400,500,600,700" rel="stylesheet"> 
+  <link rel="icon" href="img/favicon.png" sizes="32x32" type="image/png">
+</head>
+<body>
+  <img id="logo-entrar" src="img/logo_branco.png" alt="Logomarca Conect.Ideias">
+  <section class="container">
+    <h1>Redefina sua senha</h1>
+    <form class="formulario-entrar" action="" method="post">
+      <input type="password" name="nova-senha" placeholder="Nova senha" required>
+      <input type="password" name="nova-senha-confirmacao" placeholder="Confirmar nova senha" required>
+      <input class="btn-primario" type="submit" name="submit" value="Redefinir">
+    </form>
+  </section>
+</body>
+</html>
