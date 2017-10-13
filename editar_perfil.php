a23248c42410130575e3c80bbf8ce0c8f4614905
Kaique Borges <rique.kaique@gmail.com>
2017-10-13 10:23:58 -0300

    Estrutura básica da barra de navegação

---
+++ editar_perfil.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Conect.Ideias | Entrar</title>
+  <link rel="stylesheet" href="css/main.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i|Montserrat:400,500,600,700" rel="stylesheet"> 
+  <link rel="icon" href="img/favicon.png" sizes="32x32" type="image/png">
+</head>
+<body>
+  <nav id="navegacao">
+    <div class="wrapper"> 
+      <a href="achar_projeto.php"><img id="logo" src="img/logo.png" alt="Logo Conect.Ideias"></a>
+      <ul>
+        <li><a id="destaque-azul" href="criar_projeto.php">Criar projeto</a></li>
+        <li><a href="achar_projeto.php" class="selecionado">Achar projeto</a></li>
+        <li><a href="meus_projetos.php">Meus projetos</a></li>
+        <li><a href="notificacoes.php">Notificações</a></li>
+      </ul>
+      <div id="perfil">
+        <div id="separador"></div>
+        <a href="perfil.php">
+          <img id="avatar" src="img/placeholder_avatar.png" alt="Avatar">
+          <span id="nome">Roberto</span>
+        </a>
+        <img id="seta" src="img/icone.png">
+      </div>
+  </nav>
+</body>
+</html>
