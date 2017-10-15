ad7c3dfc86555670b4a7e846885d9145ee829533
Lucas <lucasmacielaguiar@gmail.com>
2017-10-15 16:03:24 -0200

    Criado o arquivo de conexao com o bd e adicionado paginacao na busca de projetos

---
+++ conexao_bd.php
@@ -0,0 +1,9 @@
+<?php
+	$servidor = "localhost";
+	$usuario = "root";
+	$senha = "";
+	$dbname = "conect_ideias";
+	
+	//Criar conexão
+	$conexao=mysqli_connect("$servidor", "$usuario", "$senha", "$dbname");
+?>
