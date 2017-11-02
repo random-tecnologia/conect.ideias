e23c3634f142bc894e81d164d1ae45e80b9af364
Kaique Borges <rique.kaique@gmail.com>
2017-11-02 19:56:24 -0200

    Adicionado botão editar na página ver_projeto

--- ver_projeto.php
+++ ver_projeto.php
@@ -19,6 +19,7 @@ require "_header.php";
     <!-- <a href="#"><button id="btn-solicitar-acesso" class="btn-primario">Solicitar acesso</button></a> -->
     <!-- <a href="#"><button id="btn-solicitar-acesso" class="btn-secundario btn-projeto">Cancelar solicitação</button></a> -->
     <!-- <a href="#"><button id="btn-solicitar-acesso" class="btn-secundario btn-projeto">Sair do projeto</button></a> -->
+    <a href="editar_projeto.php"><button id="btn-solicitar-acesso" class="btn-secundario btn-editar-projeto"><i class="ion-edit"></i>Editar</button></a>
     <div class="clearfix"></div>
   </div>
 </div>
@@ -29,11 +30,6 @@ require "_header.php";
     <p>Esse é um projeto do professor Giovanni de desenvolvimento de software em parceria com alunos de todos os cursos para construir algo inimaginável e que vai mudar vidas e paradigmas de engenharia.</p>
     <p>Ut elementum tempor molestie. Pellentesque vel lectus iaculis, vestibulum diam ac, aliquam purus. Maecenas eu fringilla magna. Maecenas id tempus purus, vel aliquam quam. Phasellus sapien nibh, pulvinar placerat accumsan ac, mollis vel neque. Morbi ultrices mauris id tortor accumsan luctus. Curabitur nec risus maximus, hendrerit nisi eu, laoreet massa.</p> 
     <p>Morbi malesuada dolor quis ligula convallis, eget placerat nisl egestas. Cras eget bibendum tellus. Integer sit amet lacus placerat metus aliquam mollis gravida eu est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse pretium lobortis leo, quis euismod felis malesuada non. Quisque tincidunt lacus urna, sit amet blandit nisi suscipit quis. Phasellus porttitor eros elit, vel aliquet mauris porttitor ut. Aenean eu enim a ligula rhoncus tempor sit amet id lorem.</p>
-    <h2>Contato</h2>
-    <ul>
-      <li>robertolopes@servidor.com</li>
-      <li>(61) 95555-5555</li>
-    </ul>
   </div>
 </section>
 
