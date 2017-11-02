9c74495146e54d62cc5ce59011b88af9d451fac3
Kaique Borges <rique.kaique@gmail.com>
2017-11-02 21:20:15 -0200

    Página de solicitações criada

---
+++ solicitacoes.php
@@ -0,0 +1,59 @@
+<?php 
+$titulo_pagina = "Projeto de Desenvolvimento de Software";
+$nome_arquivo = basename(__FILE__, ".php");
+require "_header.php";
+require "_projeto.php";
+?>
+
+<div class="wrapper">
+	<section class="container container-lista solicitacoes">
+		<ul>
+            <li class="solicitacao">
+            	<a href="#" class="avatar"><img src="img/avatar_kaique.jpg" alt="Roberto Lopes"></a>
+                <a href="#" class="identificacao">
+                	<h3 class="nome">Roberto</h3>
+                	<div class="tipo-ajuda">
+	                	<span>Criação</span>
+	                	<span>Consultoria</span>
+                	</div>
+                <div class="acoes">
+                	<a href="#">Aceitar</a>
+                	<a href="#">Rejeitar</a>
+                	<a href="#">Ver perfil</a>
+                </div>
+            </li>
+
+            <li class="solicitacao">
+            	<a href="#" class="avatar"><img src="img/avatar_kaique.jpg" alt="Roberto Lopes"></a>
+                <a href="#" class="identificacao">
+                	<h3 class="nome">Roberto</h3>
+                	<div class="tipo-ajuda">
+	                	<span>Criação</span>
+	                	<span>Consultoria</span>
+                	</div>
+                <div class="acoes">
+                	<a href="#">Aceitar</a>
+                	<a href="#">Rejeitar</a>
+                	<a href="#">Ver perfil</a>
+                </div>
+            </li>
+
+            <li class="solicitacao">
+            	<a href="#" class="avatar"><img src="img/avatar_kaique.jpg" alt="Roberto Lopes"></a>
+                <a href="#" class="identificacao">
+                	<h3 class="nome">Roberto</h3>
+                	<div class="tipo-ajuda">
+	                	<span>Criação</span>
+	                	<span>Consultoria</span>
+                	</div>
+                <div class="acoes">
+                	<a href="#">Aceitar</a>
+                	<a href="#">Rejeitar</a>
+                	<a href="#">Ver perfil</a>
+                </div>
+            </li>
+        </ul>
+	</section>    
+</div>
+
+<?php require "_footer.php" ?>
