93ada7fdfeaafb931a1f8415e943f1b2ca3a6a4b
Geraldo Victor <geraldov932@gmail.com>
2017-11-07 14:08:52 -0200

    atualização dos bugs

--- atualizar_cadastro.php
+++ atualizar_cadastro.php
@@ -2,31 +2,35 @@
 
 $conn = mysqli_connect("localhost", "root", "", "conect_ideias"); //para fazer a conexão com o banco de dados
 
-$id_usuario = 1;
+session_start();
+
+$id_usuario = $_SESSION['id_usuario'];
 
 $consulta = "select email, senha from usuarios where id like $id_usuario";//seleciona de onde vc quer tirar os dados
 
 $resultado = mysqli_query($conn, $consulta);//faz a consulta e armazena num array
 
 if(isset($_POST["submit"])){ // comando abaixo significa que só será executado se houver preenchimento do campo submit
-	$email = $_POST["email"];
 	$senha = $_POST["senha"];
 	$nova_senha = $_POST["nova_senha"];
 
-	while ($row = mysqli_fetch_array($resultado)){// armazena temporariamente os dados armazenados 
-		 $row["email"]."<br />";
+	while ($row = mysqli_fetch_array($resultado)){// armazena temporariamente os dados do banco
 		 $row["senha"];
 		 if ($senha === $row["senha"]){
 		 	if ($nova_senha === $senha || $nova_senha === $row["senha"]){
 		 		echo "ESSA JÁ É A SUA SENHA!";
 		 	}
 		 	else {
-		 			$consulta2 = "update usuarios set senha = '$nova_senha'";
+		 			$consulta2 = "UPDATE usuarios set senha = '$nova_senha' WHERE id = $id_usuario";
 		 			mysqli_query ($conn, $consulta2);
+		 			header("location: perfil.php");
+		 			exit();
 		 	}
 		 }
-		 else 
+		 else {
 		 	echo "SENHA INCORRETA";
+		 }
+	
 	}
 
 }	
@@ -39,9 +43,8 @@ if(isset($_POST["submit"])){ // comando abaixo significa que só será executado
 </head>
 <body>
 <form method="post" action="atualizar_cadastro.php">
-	<input type="text" name="email" placeholder="E-mail"><br> 
-	<input type="password" name="senha" placeholder="Senha atual"><br>
-	<input type="password" name="nova_senha" placeholder="Nova senha"><br>
+	<input type="password" name="senha" placeholder="Senha atual" required><br>
+	<input type="password" name="nova_senha" placeholder="Nova senha" required><br>
 	<input type="submit" name="submit" value="Enviar">
 </form>
 <a href="excluir_conta.php">Excluir conta</a>
--- perfil.php
+++ perfil.php
@@ -1,12 +1,29 @@
 <?php  
 $conn = mysqli_connect("localhost", "root", "", "conect_ideias");
-$id_usuário = 2;
-$consulta = "select descricao,avatar,telefone from usuarios where id = $id_usuário";
+session_start();
+
+$id_usuario = $_SESSION['id_usuario'];
+
+if(isset($_GET['id'])){
+	$id = $_GET['id'];
+	$consulta = "select descricao,avatar,telefone from usuarios where id = $id";
+	$resultado = mysqli_query($conn, $consulta);
+while ($row = mysqli_fetch_array($resultado)){// armazena temporariamente os dados do banco
+		 $desc = $row["descricao"]."</br >";
+		 $ava = $row["avatar"]."</br >";
+		 $tel = $row["telefone"];
+		 echo "$desc $ava $tel";
+	}
+}
+
+else{
+$consulta = "select descricao,avatar,telefone from usuarios where id = $id_usuario";
 $resultado = mysqli_query($conn, $consulta);
 while ($row = mysqli_fetch_array($resultado)){// armazena temporariamente os dados do banco
 		 $desc = $row["descricao"]."</br >";
 		 $ava = $row["avatar"]."</br >";
 		 $tel = $row["telefone"];
-		 echo "$desc, $ava, $tel";
+		 echo "$desc $ava $tel";
+	}
 }
 ?>
